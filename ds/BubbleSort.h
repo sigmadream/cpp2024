fce52d787f9343e9fc4c3257ff4691fd71202dad
Sangkon Han <sigmadream@gmail.com>
2024-10-16 14:50:47 +0900

    feat: BubbleSort

---
+++ ds/BubbleSort.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+
+class BubbleSort {
+public:
+    static void sort(std::vector<int>& arr);
+
+private:
+    BubbleSort() = delete;
+};
---
+++ unittest/test.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include <vector>
+#include <algorithm>
+#include <gtest/gtest.h>
+#include "../ds/BubbleSort.h"
+
+TEST(BubbleSortTest, SortsCorrectly) {
+    std::vector<int> arr = { 64, 34, 25, 12, 22, 11, 90 };
+    std::vector<int> expected = arr;
+    std::sort(expected.begin(), expected.end());
+
+    BubbleSort::sort(arr);
+
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(BubbleSortTest, HandlesSingleElement) {
+    std::vector<int> arr = { 42 };
+    BubbleSort::sort(arr);
+    EXPECT_EQ(arr, std::vector<int>{42});
+}
+
+TEST(BubbleSortTest, HandlesReverseSortedVector) {
+    std::vector<int> arr = { 5, 4, 3, 2, 1 };
+    std::vector<int> expected = { 1, 2, 3, 4, 5 };
+    BubbleSort::sort(arr);
+    EXPECT_EQ(arr, expected);
+}
